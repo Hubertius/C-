520879143aa7b9a681bec94731ccc99405f9c672
Hubertius <hubert.kopec@interia.pl>
2020-06-13 01:14:23 +0200

    cin.clear() i continue

---
+++ 6.14 cingolf/Źródło.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	const int Max = 5;
+	int golf[Max];
+	cout << "Prosze podac 5 swoich wynikow w golfie.\n";
+	for (int i = 0; i < Max; i++)
+	{
+		if( !(cin >> golf[i]) )
+		{
+			cout << "Incorrect input!\n";
+			cin.clear(); // Zerowanie obiektu cin, aby w przyszłości chodził poprawnie.
+			while (cin.get() != '\n')
+			{
+				continue;
+			}
+			i--;
+		}
+	}
+
+	return 0;
+}
