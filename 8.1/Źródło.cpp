fddfaea36675f68c09d8018c0c27b5b196589578
Hubertius <hubert.kopec@interia.pl>
2020-08-02 12:52:59 +0200

    Square , inline

---
+++ 8.1/Źródło.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+inline double square(double x) { return x * x; }
+
+int main()
+{
+	double a, b;
+	double c = 13.0;
+	a = square(5.0);
+	b = square(4.5 + 7.5);
+	cout << "a = " << a << " b = " << b << endl;
+	cout << "c = " << c << endl;
+	cout << "c^2 = " << square(c);
+	return 0;
+}
